fe7ebc0cfca7845ac5f873b071a78a45abfa650a
Westley Feller <feller.westley@gmail.com>
2019-10-02 22:30:18 -0700

    Refactored card into MovieCard component

--- client/src/Movies/MovieList.jsx
+++ client/src/Movies/MovieList.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
 
 
-const MovieList = (props) => {
+const MovieList = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
@@ -24,52 +24,10 @@ const MovieList = (props) => {
   return (
     <div className="movie-list">
       {movies.map((movie) => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   );
-};
-
-function MovieDetails({ movie }) {
-  const {
-    title, director, metascore, stars,
-  } = movie;
-  return (
-    <Link to={`/movie/${movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-        Director:
-
-          <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-        Metascore:
-
-          <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map((star) => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-    </Link>
-  );
-}
-
-MovieDetails.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.number,
-    title: PropTypes.string,
-    director: PropTypes.string,
-    metascore: PropTypes.number,
-    stars: PropTypes.arrayOf(
-      PropTypes.string,
-    ),
-  }).isRequired,
 };
 
 export default MovieList;
