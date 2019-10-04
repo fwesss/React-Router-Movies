a8012c7eaddeb9462ca37853ae840f620c5262b0
Westley Feller <feller.westley@gmail.com>
2019-10-03 20:51:18 -0700

    Changed saved list links into NavLinks

--- client/src/Movies/SavedList.jsx
+++ client/src/Movies/SavedList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 
@@ -7,7 +7,7 @@ const SavedList = ({ list }) => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {list.map((movie) => (
-      <Link to={`/movie/${movie.id}`} className="saved-movie" key={movie.id}>{movie.title}</Link>
+      <NavLink to={`/movie/${movie.id}`} className="saved-movie" key={movie.id}>{movie.title}</NavLink>
     ))}
     <Link to="/" className="home-button">Home</Link>
   </div>
